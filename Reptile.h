14814e2163a7baf0a441081776860a68ae9de1b4
logienshawky <lojyshawky@gmail.com>
2025-11-12 15:54:10 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,16 @@
-#ifndef REPTILE_H
-#define REPTILE_H
-
-#include "Animal.h"
-
-//define reptile class here
-
-
-
-
-
-
-
-
-
+#ifndef Reptile_H
+#define Reptile_H
+#include <iostream>
+#include <string>
+using namespace std;
+class Reptile: public Animal{
+    private:
+    bool isV;
+    public:
+    float getW();
+    void setV(bool v);
+    Reptile();
+    Reptile(string n,int a,bool b,bool v);
+    ~Reptile();
+};
 #endif
