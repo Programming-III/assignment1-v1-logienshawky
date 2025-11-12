d4c4964aaa0b22ff4812c5c83d4d2f50e51d3c9c
logienshawky <lojyshawky@gmail.com>
2025-11-12 15:52:44 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,16 @@
 #ifndef BIRD_H
 #define BIRD_H
-
-#include "Animal.h"
-
-//define class bird here 
-
-
-
-
-
-
-
+#include <iostream>
+#include <string>
+using namespace std;
+class Bird: public Animal{
+    private:
+    float wing;
+    public:
+    float getW();
+    void setW(float w);
+    Bird();
+    Bird(string n,int a,bool b,float w);
+    ~Bird();
+};
 #endif
